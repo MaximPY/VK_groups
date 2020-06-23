8d2b55a5c5d139f4288e34a89a19efb3e9f7f155
Maxim Pyanov <pyanovmaxim95@gmail.com>
2020-06-23 23:23:45 +0200

    Initial commit

---
+++ VK_groups.py
@@ -0,0 +1,26 @@
+import requests
+# (eshmargunov) и id (171691064)
+params1 = {
+    'access_token': '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
+    'user_id': '171691064',
+    'v': '5.110'
+}
+resp1 = requests.get(url='https://api.vk.com/method/groups.get/', params= params1)
+
+group_id_list = resp1.json()['response']['items']
+# print(id_list)
+# print(resp.json())
+
+params2= {
+    'access_token': '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
+    'group_id': group_id_list[0],
+    'v': '5.110',
+    'fields': '',
+    'filter': 'friends'
+
+}
+resp2 = requests.get(url= 'https://api.vk.com/method/groups.getMembers', params= params2)
+# 171691064
+print(resp2.json())
+# Создай репозиторий на гите, чтобы туда загружать файлы
+# Проверять человека в группе невыгодно - надо группу у человека
